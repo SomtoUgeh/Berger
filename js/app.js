2078eb3e4bfcf82b3ae08f6629a60230cca29854
UGEH <smugeh@gmail.com>
2018-02-21 08:36:31 +0100

    Minified CSS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-
 // Global Variables & Default Locations
 var map;
-var clientID;
-var clientSecret;
 
 // Default location for several points
 var defaultLocations = [
@@ -51,52 +48,15 @@ var defaultLocations = [
 ];
 
 
-// Working with the foursquare API
  var Location = function(data) {
     var self = this;
     this.name = data.title;
     this.position = data.location;
-    this.URL = '';
-    this.street = '';
-    this.city = '';
-    this.phone = '';
-
-    this.visible = ko.observable(true);
-
-    // Credentials for FOURSQUARE Api
-    clientID = 'AEDY4G2EDEFQZUOPU1RZNVMOUX0X25LCTKM0SJQWQPQG24L3';
-    clientSecret = '5Y2UT2PWZRKVXBM0GJ5DBGRAXFK02M45XZVLAMYQSZMQVZWT';
-
-    // Foursquare API endpoint build
-    var fourSquareURL =  'https://api.foursquare.com/v2/venues/search?ll=' + this.position.lat + ',' + this.position.lng + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20170801' + '&query=' + this.name;
-
-    // Gets the data from foursquare and store it into its' own variables.
-    $.getJSON(fourSquareURL).done(function (data) {
-        var results = data.response.venues[0];
-        self.URL = results.url;
-        if (typeof self.URL === 'undefined') {
-            self.URL = "";
-        }
-
-        self.street = results.location.formattedAddress[0];
-        self.city = results.location.formattedAddress[1];
-        self.phone = results.contact.phone;
-        if (typeof self.phone === 'undefined') {
-            self.phone = '';
-        }
-
-    }).fail(function () {
-        $('.venue-list').html('There was an error with the Foursquare API call. Please refresh the page and try again');
-    });
 
     // This is what the infowindow will contain.
     this.contentString =
         '<div class="info-window-content">' +
         '<div class="title"><b>' + data.title + "</b></div>" +
-        '<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
-        '<div class="content">' + self.street + "</div>" +
-        '<div class="content">' + self.city + "</div>" +
-        '<div class="content">' + self.phone + "</div>" +
         "</div>";
 
     // Puts the content string inside Infowindow.
@@ -124,10 +84,6 @@ var defaultLocations = [
         self.contentString =
             '<div class="info-window-content">' +
             '<div class="title"><b>' + data.title + "</b></div>" +
-            '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
-            '<div class="content">' + self.street + "</div>" +
-            '<div class="content">' + self.city + "</div>" +
-            '<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div>" +
             "</div>";
 
         self.infoWindow.setContent(self.contentString);
@@ -169,7 +125,7 @@ function ViewModel(){
         self.locationList.push( new Location(locationItem));
     });
 
-    // Searches for what user typed in the input bar using the locationlist array.
+    // Searches for what user typed in the input bar using the location list array.
     // Only displaying the exact item results that user type if available in the locationlist array.
     this.filteredList = ko.computed( function() {
         var filter = self.searchTerm().toLowerCase();
